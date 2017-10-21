aaee91ad152ccfa0f17cac6f3b9af9e233a001bd
exik92 <extreme2512@gmail.com>
2017-10-21 23:41:40 +0200

    First commit

---
+++ src/main/java/pl/edu/uwb/server/controller/SpecjalizationController.java
@@ -0,0 +1,43 @@
+package pl.edu.uwb.server.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.edu.uwb.server.entity.Specialization;
+import pl.edu.uwb.server.repository.SpecializationDao;
+
+@RestController
+@RequestMapping("/specs")
+public class SpecjalizationController {
+
+	/** The Log4j2 logger. */
+	private static Logger logger = LogManager.getLogger(SpecjalizationController.class);
+
+	private final SpecializationDao specializationDao;
+
+	@Autowired
+	public SpecjalizationController(SpecializationDao specializationDao) {
+		this.specializationDao = specializationDao;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public boolean addNewSpecialization(@RequestParam String name) {
+		logger.debug("addNewSpecialization");
+		return specializationDao.addNewSpecialization(name);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Specialization> findAllSpecializations() {
+		logger.debug("findAllSpecializations");
+		return specializationDao.findAllSpecializations();
+	}
+
+}
---
+++ src/main/java/pl/edu/uwb/server/repository/SpecializationDao.java
@@ -0,0 +1,60 @@
+package pl.edu.uwb.server.repository;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+
+import pl.edu.uwb.server.entity.Specialization;
+import pl.edu.uwb.server.util.SessionConnection;
+
+
+/**
+ * The Class SpecializationDao.
+ * 
+ * @author acewiczm
+ */
+@Component
+public class SpecializationDao {
+
+	/** The Log4j2 logger. */
+	private static Logger logger = LogManager.getLogger(SpecializationDao.class);
+
+	/**
+	 * Adds the new specialization.
+	 *
+	 * @param name
+	 *            the name
+	 * @return true, if successful
+	 */
+	public boolean addNewSpecialization(String name) {
+		logger.debug("addNewSpecialization");
+		Session session = SessionConnection.getSessionFactory().openSession();
+		session.beginTransaction();
+		Specialization specialization = new Specialization(name);
+		session.save(specialization);
+		session.getTransaction().commit();
+		SessionConnection.shutdown(session);
+		logger.info("User registered correctly.");
+		return true;
+	}
+
+	/**
+	 * Find all specializations.
+	 *
+	 * @return the list
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Specialization> findAllSpecializations() {
+		logger.debug("findAllSpecializations");
+		List<Specialization> specs;
+		Session session = SessionConnection.getSessionFactory().openSession();
+		specs = session.createQuery("from Specialization").list();
+		SessionConnection.shutdown(session);
+		logger.info("All users listed.");
+		return specs;
+	}
+
+}
